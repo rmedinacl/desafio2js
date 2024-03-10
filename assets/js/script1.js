ddc2e432a9114b1b398c34bdfd514466db320937
Rodrigo Medina P <rmedina@mountain-creative.com>
2024-03-10 10:58:02 -0300

    cambios estructura js

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,14 +1,13 @@
 //Ejercicio 1
 
 document.addEventListener("DOMContentLoaded", function () {
+  const modificarBorde = () => {
+    const foto_dona = document.getElementById("img_dona");
+    if (foto_dona.style.border === "2px solid red") {
+      foto_dona.style.border = "";
+    } else {
+      foto_dona.style.border = "2px solid red";
+    }
+  };
   document.querySelector("#img_dona").addEventListener("click", modificarBorde);
 });
-
-const modificarBorde = () => {
-  const foto_dona = document.getElementById("img_dona");
-  if (foto_dona.style.border === "2px solid red") {
-    foto_dona.style.border = "";
-  } else {
-    foto_dona.style.border = "2px solid red";
-  }
-};
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,41 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const ingresarBtn = document.getElementById("ingresar-btn");
-  ingresarBtn.addEventListener("click", ingresar);
-});
+  const verificar = () => {
+    const cant1 = parseInt(document.getElementById("cantidad_1").value);
+    const cant2 = parseInt(document.getElementById("cantidad_2").value);
+    const cant3 = parseInt(document.getElementById("cantidad_3").value);
 
-document.addEventListener("DOMContentLoaded", function () {
+    if (
+      !validarNumero(cant1) ||
+      !validarNumero(cant2) ||
+      !validarNumero(cant3)
+    ) {
+      document.getElementById("respuesta").innerHTML =
+        "Los números no son válidos. Ingrese solo números entre 1 y 10. ";
+      return;
+    }
+
+    const total = cant1 + cant2 + cant3;
+
+    if (total > 10) {
+      document.getElementById("respuesta").innerHTML =
+        "Llevas demasiados stickers. El máximo es 10";
+      return;
+    }
+
+    document.getElementById("respuesta").innerHTML =
+      "Llevas " + total + " stickers";
+  };
+
+  const validarNumero = (numero) => {
+    return !isNaN(numero) && numero > 0 && numero <= 10;
+  };
+
   document
     .getElementById("boton-verificar")
     .addEventListener("click", verificar);
 });
-
-const verificar = () => {
-  const cant1 = parseInt(document.querySelector("#cantidad_1").value);
-  const cant2 = parseInt(document.querySelector("#cantidad_2").value);
-  const cant3 = parseInt(document.querySelector("#cantidad_3").value);
-
-  if (!validarNumero(cant1) || !validarNumero(cant2) || !validarNumero(cant3)) {
-    document.querySelector("#respuesta").innerHTML =
-      "Ingrese solo números entre 1 y 10";
-    return;
-  }
-
-  const total = cant1 + cant2 + cant3;
-
-  if (total > 10) {
-    document.querySelector("#respuesta").innerHTML =
-      "Llevas demasiados stickers. El máximo es 10";
-    return;
-  }
-
-  document.querySelector("#respuesta").innerHTML =
-    "Llevas " + total + " stickers";
-};
-
-const validarNumero = (numero) => {
-  return numero > 0 && numero <= 10;
-};
-
-document
-  .getElementById("#boton_verificar")
-  .addEventListener("click", verificar);
